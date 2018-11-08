e39d6f2ebbe7acab1ed3d9b2be6cb3b9e730e395
slorulz <kevin.cerneka@gmail.com>
2018-11-08 10:16:46 +0100

    First clone commit

--- Zapis_na_SD_kartico/src/main.cpp
+++ Zapis_na_SD_kartico/src/main.cpp
@@ -1,43 +1,43 @@
 #include <Arduino.h>
 // knjiznice SD
-#include "FS.h"
-#include "SD.h"
+#include <FS.h>
+#include <SD.h>
 #include <SPI.h>
 
 // SD card pin
 #define SD_CS 5
 
-File besedilo;
+File tekst;
 
 
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
 
  while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
   Serial.print("Initializing SD card...");
 
-  if (!SD.begin(5)) {
+  if (!SD.begin(SD_CS)) {
     Serial.println("initialization failed!");
     while (1);
   }
   Serial.println("initialization done.");
 
- besedilo = SD.open("besedilo.txt", FILE_WRITE);
+  tekst = SD.open("/tekst.txt", FILE_WRITE);
 
  // if the file opened okay, write to it:
-  if (besedilo) {
-    Serial.print("Writing to besedilo.txt...");
-    besedilo.println("Hello world");
+  if (tekst) {
+    Serial.print("Writing to tekst.txt...");
+    tekst.println("Hello world");
     // close the file:
-    besedilo.close();
+    tekst.close();
     Serial.println("done.");
   } else {
     // if the file didn't open, print an error:
-    Serial.println("error opening besedilo.txt");
+    Serial.println("error opening tekst.txt");
   }
 
 
